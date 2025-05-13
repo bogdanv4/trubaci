bc134c6a40521e48137999f131541dae8600da33
Bogdan <vujic.bogdan@gmail.com>
2025-05-13 22:40:50 +0200

    Added form validation and functionality
    Minor design changes

--- js/script.js
+++ js/script.js
@@ -1,29 +1,75 @@
-$(document).ready(function () {
-  // Initialize Tooltip
-  $('[data-toggle="tooltip"]').tooltip();
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const messageInput = document.querySelector("#comments");
+const errors = document.querySelectorAll(".error");
+const confirmation = document.querySelector(".confirm-sent");
+const submitButton = document.querySelector("#submitBtn");
 
-  // Add smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a[href='#myPage']").on("click", function (event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+const validateEmail = function (email) {
+  const pattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
-      // Store hash
-      var hash = this.hash;
+  if (!email.match(pattern)) errors[1].style.opacity = 1;
+  else return true;
+};
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-      $("html, body").animate(
-        {
-          scrollTop: $(hash).offset().top,
-        },
-        900,
-        function () {
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        }
-      );
-    } // End if
-  });
+const validateName = function (firstName) {
+  if (firstName.length < 3) errors[0].style.opacity = 1;
+  else return true;
+};
+
+const validateMessage = function (message) {
+  if (message.length < 10) errors[2].style.opacity = 1;
+  else return true;
+};
+
+const clearFields = function () {
+  nameInput.value = "";
+  emailInput.value = "";
+  messageInput.value = "";
+};
+
+const validAll = function (validateName, validateEmail, validateMessage) {
+  if (validateName && validateEmail && validateMessage) {
+    console.log(`Ime: ${emailInput.value}
+        email: ${emailInput.value} 
+        poruka: ${messageInput.value}`);
+    clearFields();
+  }
+};
+
+const clearError = function () {
+  errors.forEach((error) => (error.style.opacity = 0));
+};
+
+const sendMail = function (name, email, message) {
+  let params = {
+    name,
+    email,
+    message,
+  };
+
+  emailjs.send("service_i8qxnce", "template_4sqgokw", params).then(
+    (response) => {
+      console.log("SUCCESS!", response.status, response.text);
+    },
+    (error) => {
+      console.log("FAILED...", error);
+    }
+  );
+};
+
+submitButton.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  clearError();
+
+  if (validateName && validateEmail && validateMessage) {
+    console.log(`Ime: ${nameInput.value}
+        email: ${emailInput.value} 
+        poruka: ${messageInput.value}`);
+    sendMail(nameInput.value, emailInput.value, messageInput.value);
+    confirmation.style.opacity = 1;
+
+    clearFields();
+  }
 });
